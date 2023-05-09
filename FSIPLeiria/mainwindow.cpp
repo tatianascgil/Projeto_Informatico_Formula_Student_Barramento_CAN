8740dcf7923f725460f4c09b53a1af474d2a5856
Diogo Pereira <70278296+Nerddio@users.noreply.github.com>
2023-05-09 17:40:44 +0100

    ReadCsvXlsFixed

--- FSIPLeiria/mainwindow.cpp
+++ FSIPLeiria/mainwindow.cpp
@@ -137,7 +137,25 @@ struct MyData {
         QList<QList<QString>> rows;
         while (!stream.atEnd()) {
             QString line = stream.readLine();
-            QStringList values = line.split(";");
+            QStringList values;
+            QChar delimiter;
+            for (int i = 0; i < line.length(); ++i) {
+                QChar c = line.at(i);
+                if (c == ',' || c == ';' || c == '\t') {
+                    if (!delimiter.isNull() && delimiter != c) {
+                        // If multiple delimiters are used in the same line, assume the most frequently used delimiter
+                        // as the actual delimiter.
+                        break;
+                    }
+                    delimiter = c;
+                }
+            }
+            if (delimiter.isNull()) {
+                // No delimiter found, treat the whole line as a single value.
+                values.append(line);
+            } else {
+                values = line.split(delimiter);
+            }
             if (headers.isEmpty()) {
                 headers = values;
             } else {
@@ -197,7 +215,7 @@ void MainWindow::on_btnSaveFile_clicked()
     // Create a QFileDialog object to allow the user to select the output file location and name.
     QFileDialog fileDialog;
     fileDialog.setAcceptMode(QFileDialog::AcceptSave);
-    fileDialog.setNameFilter(tr("Excel files (.xlsx);;Binary Files (.bin)"));
+    fileDialog.setNameFilter(tr("Excel files (.xlsx);;CSV files (.csv);;Binary files (.bin)"));
     fileDialog.setDefaultSuffix("xlsx");
     QString fileName = fileDialog.getSaveFileName();
 
@@ -206,25 +224,56 @@ void MainWindow::on_btnSaveFile_clicked()
         return;
     }
 
-    // Create a QXlsx::Document object to represent the Excel file.
-    QXlsx::Document xlsxDoc(fileName);
-
-    // Add a new worksheet to the Excel document.
-    xlsxDoc.addSheet("Sheet1");
-
-    // Traverse the QTableView and add the table data to the Excel worksheet.
-    QAbstractItemModel* model = ui->tableView->model();
-    for (int row = 0; row < model->rowCount(); ++row) {
-        for (int col = 0; col < model->columnCount(); ++col) {
-            QModelIndex index = model->index(row, col);
-            QVariant data = model->data(index);
-            if (data.isValid()) {
-                xlsxDoc.write(row + 1, col + 1, data.toString());
+    // Determine the file format based on the selected file extension.
+    QString fileExt = QFileInfo(fileName).suffix();
+    if (fileExt == "csv") {
+        // Save to a CSV file.
+        QFile csvFile(fileName);
+        if (csvFile.open(QIODevice::WriteOnly | QIODevice::Append | QIODevice::Text)) {
+            QTextStream csvStream(&csvFile);
+            QAbstractItemModel* model = ui->tableView->model();
+            for (int row = 0; row < model->rowCount(); ++row) {
+                for (int col = 0; col < model->columnCount(); ++col) {
+                    QModelIndex index = model->index(row, col);
+                    QVariant data = model->data(index);
+                    if (data.isValid()) {
+                        csvStream << data.toString() << ",";
+                    }
+                }
+                csvStream << "\n";
+            }
+            csvFile.close();
+        }
+    } else if (fileExt == "xls") {
+        // Save to an XLS file using the QXlsx library.
+        QXlsx::Document xlsDoc;
+        xlsDoc.addSheet("Sheet1");
+        QAbstractItemModel* model = ui->tableView->model();
+        for (int row = 0; row < model->rowCount(); ++row) {
+            for (int col = 0; col < model->columnCount(); ++col) {
+                QModelIndex index = model->index(row, col);
+                QVariant data = model->data(index);
+                if (data.isValid()) {
+                    xlsDoc.write(row + 1, col + 1, data.toString());
+                }
             }
         }
+        xlsDoc.saveAs(fileName);
+    } else {
+        // Save to an XLSX file (default).
+        QXlsx::Document xlsxDoc(fileName);
+        xlsxDoc.addSheet("Sheet1");
+        QAbstractItemModel* model = ui->tableView->model();
+        for (int row = 0; row < model->rowCount(); ++row) {
+            for (int col = 0; col < model->columnCount(); ++col) {
+                QModelIndex index = model->index(row, col);
+                QVariant data = model->data(index);
+                if (data.isValid()) {
+                    xlsxDoc.write(row + 1, col + 1, data.toString());
+                }
+            }
+        }
+        xlsxDoc.saveAs(fileName);
     }
-
-    // Save the Excel document to the output file.
-    xlsxDoc.saveAs(fileName);
 }
 
