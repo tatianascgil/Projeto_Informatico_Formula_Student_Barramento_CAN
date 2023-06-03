52e1564b76b48121b32f230634365a04c5fc2b50
tatianascgil <47739867+tatianascgil@users.noreply.github.com>
2023-06-03 16:49:17 +0100

    CARROVRUMVRUM

---
+++ FSIPLeiria/criarcarro.cpp
@@ -0,0 +1,131 @@
+#include "criarcarro.h"
+#include "ui_criarcarro.h"
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QDir>
+#include <QDebug>
+#include <QMessageBox>
+#include <QFile>
+#include <QTextStream>
+#include <QColor>
+
+CriarCarro::CriarCarro(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::CriarCarro)
+{
+    ui->setupUi(this);
+}
+
+CriarCarro::~CriarCarro()
+{
+    delete ui;
+}
+
+void CriarCarro::on_btnCancelar_clicked()
+{
+    MainWindow *mainWindow = new MainWindow();
+    mainWindow->show();
+    this->close();
+}
+
+
+void CriarCarro::on_btnCriarCarro_clicked()
+{
+
+    QString nomeCarro = ui->textEditNomeCarro->toPlainText();
+    QString tipoCarro = ui->btnTipoCarro->currentText();
+    QString obsCarro = ui->textEditObsCarro->toPlainText();
+
+//    // Emit the carNameEntered signal after a delay
+//    QMetaObject::invokeMethod(this, [this, nomeCarro]() {
+//            emit carNameEntered(nomeCarro);
+//        }, Qt::QueuedConnection);;
+
+    bool nomeCarroEmpty = nomeCarro.trimmed().isEmpty();
+    bool tipoCarroEmpty = tipoCarro.isEmpty();
+
+    if (nomeCarroEmpty && tipoCarroEmpty) {
+        QMessageBox::critical(this, "Erro", "É obrigatório preencher os campos 'Nome' e 'Tipo'!");
+        return;
+    } else if (nomeCarroEmpty) {
+        QMessageBox::critical(this, "Erro", "É obrigatório preencher o campo 'Nome'!");
+        return;
+    } else if (tipoCarroEmpty) {
+        QMessageBox::critical(this, "Erro", "É obrigatório preencher o campo 'Tipo'!");
+        return;
+    }
+
+    if(nomeCarro.contains(";")){
+        QMessageBox::critical(this, "Erro", "É proíbido utilizar semi-vírgulas ';'!");
+        return;
+    }
+
+    QString folderName = nomeCarro.trimmed();
+     folderName.replace(" ", "");
+
+    QString currentPath = QDir::currentPath();
+    QString targetDir = currentPath + "/../FSIPLeiria/settings";
+    QString folderPath = targetDir + "/" + folderName;
+
+    QDir dir(targetDir);
+    if (!dir.exists()) {
+        if (dir.mkpath(targetDir)) {
+        } else {
+            QMessageBox::critical(this, "Error", "Failed to create target directory!");
+            return;
+        }
+    }
+
+    QDir folderDir(folderPath);
+    if (!folderDir.exists()) {
+        if (folderDir.mkpath(folderPath)) {
+            QMessageBox::information(this, "Successo", "Pasta " + folderName + " criada com sucesso!");
+
+//            // Emit the signal with the car name
+//            emit carNameEntered(nomeCarro);
+
+//            // Set the selected value of the QComboBox
+//            MainWindow* mainWindow = qobject_cast<MainWindow*>(parent());
+//            if (mainWindow) {
+//                mainWindow->setComboBoxSelectedValue(nomeCarro);
+//            }
+
+            // Saving the car data in a TXT file
+            QString filePath = folderPath + "/caracteristicas.txt";
+            QFile file(filePath);
+            if (file.open(QIODevice::WriteOnly | QIODevice::Text)) {
+                QTextStream stream(&file);
+
+                // Write the data
+                QString nome = nomeCarro;
+                QString tipo = tipoCarro;
+                QString observacoes = obsCarro;
+                stream << nome << ";" << tipo << ";" << observacoes << "\n";
+
+                file.close();
+
+                QMessageBox::information(this, "Guardar Dados", "Dados salvados com sucesso!");
+
+
+                MainWindow *mainWindow = new MainWindow();
+                mainWindow->show();
+                this->close();
+            } else {
+                QMessageBox::critical(this, "Erro", "Erro ao guardar os dados!");
+            }
+        }
+    } else {
+        QMessageBox::information(this, "Erro", "Erro. A pasta " + folderName + " já existe!");
+    }
+
+}
+
+
+void CriarCarro::on_commandButtonVoltar_clicked()
+{
+    MainWindow *mainWindow = new MainWindow();
+    mainWindow->show();
+    this->close();
+}
+
---
+++ build-FSIPLeiria-Desktop_Qt_6_5_0_MinGW_64_bit-Debug/ui_vercarro.h
@@ -0,0 +1,160 @@
+/********************************************************************************
+** Form generated from reading UI file 'vercarro.ui'
+**
+** Created by: Qt User Interface Compiler version 6.5.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_VERCARRO_H
+#define UI_VERCARRO_H
+
+#include <QtCore/QVariant>
+#include <QtGui/QIcon>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QCommandLinkButton>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QScrollArea>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_VerCarro
+{
+public:
+    QPushButton *btnDefinicoes;
+    QPushButton *btnEstatistica;
+    QLabel *label;
+    QLabel *label_2;
+    QLabel *label_3;
+    QLabel *labelNomeCarro;
+    QLabel *labelTipoCarro;
+    QScrollArea *scrollArea;
+    QWidget *scrollAreaWidgetContents;
+    QLabel *labelObsCarro;
+    QCommandLinkButton *commandButtonVoltar;
+
+    void setupUi(QWidget *VerCarro)
+    {
+        if (VerCarro->objectName().isEmpty())
+            VerCarro->setObjectName("VerCarro");
+        VerCarro->resize(712, 338);
+        QIcon icon;
+        icon.addFile(QString::fromUtf8("../icon.ico"), QSize(), QIcon::Normal, QIcon::Off);
+        VerCarro->setWindowIcon(icon);
+        VerCarro->setStyleSheet(QString::fromUtf8("\n"
+"	background-color:white"));
+        btnDefinicoes = new QPushButton(VerCarro);
+        btnDefinicoes->setObjectName("btnDefinicoes");
+        btnDefinicoes->setGeometry(QRect(530, 60, 161, 51));
+        QFont font;
+        font.setPointSize(10);
+        btnDefinicoes->setFont(font);
+        btnDefinicoes->setCursor(QCursor(Qt::PointingHandCursor));
+        btnDefinicoes->setStyleSheet(QString::fromUtf8("QPushButton{\n"
+"	background-color:rgb(255, 245, 166);\n"
+"	border:none;\n"
+"	color:rgb(95, 91, 61);\n"
+"	border -bottom: 5px solid rgb(162, 155, 105)\n"
+"}\n"
+"QPushButton:hover{\n"
+"	background-color:rgb(229, 217, 150);\n"
+"	border-bottom: 5px solid rgb(162, 155, 105)\n"
+"}\n"
+"QPushButton:pressed {\n"
+"	background-color:rgb(255, 242, 165);\n"
+"	border-bottom: 5px solid rgb(162, 155, 105)\n"
+"}"));
+        btnEstatistica = new QPushButton(VerCarro);
+        btnEstatistica->setObjectName("btnEstatistica");
+        btnEstatistica->setGeometry(QRect(530, 140, 161, 51));
+        btnEstatistica->setFont(font);
+        btnEstatistica->setCursor(QCursor(Qt::PointingHandCursor));
+        btnEstatistica->setStyleSheet(QString::fromUtf8("QPushButton{\n"
+"	background-color:rgb(255, 245, 166);\n"
+"	border:none;\n"
+"	color:rgb(95, 91, 61);\n"
+"	border -bottom: 5px solid rgb(162, 155, 105)\n"
+"}\n"
+"QPushButton:hover{\n"
+"	background-color:rgb(229, 217, 150);\n"
+"	border-bottom: 5px solid rgb(162, 155, 105)\n"
+"}\n"
+"QPushButton:pressed {\n"
+"	background-color:rgb(255, 242, 165);\n"
+"	border-bottom: 5px solid rgb(162, 155, 105)\n"
+"}"));
+        label = new QLabel(VerCarro);
+        label->setObjectName("label");
+        label->setGeometry(QRect(60, 40, 91, 31));
+        QFont font1;
+        font1.setPointSize(16);
+        font1.setBold(true);
+        label->setFont(font1);
+        label_2 = new QLabel(VerCarro);
+        label_2->setObjectName("label_2");
+        label_2->setGeometry(QRect(60, 90, 91, 31));
+        label_2->setFont(font1);
+        label_3 = new QLabel(VerCarro);
+        label_3->setObjectName("label_3");
+        label_3->setGeometry(QRect(60, 130, 131, 31));
+        label_3->setFont(font1);
+        labelNomeCarro = new QLabel(VerCarro);
+        labelNomeCarro->setObjectName("labelNomeCarro");
+        labelNomeCarro->setGeometry(QRect(150, 40, 361, 31));
+        QFont font2;
+        font2.setPointSize(16);
+        labelNomeCarro->setFont(font2);
+        labelTipoCarro = new QLabel(VerCarro);
+        labelTipoCarro->setObjectName("labelTipoCarro");
+        labelTipoCarro->setGeometry(QRect(140, 90, 351, 31));
+        labelTipoCarro->setFont(font2);
+        scrollArea = new QScrollArea(VerCarro);
+        scrollArea->setObjectName("scrollArea");
+        scrollArea->setGeometry(QRect(60, 170, 431, 131));
+        scrollArea->setWidgetResizable(true);
+        scrollAreaWidgetContents = new QWidget();
+        scrollAreaWidgetContents->setObjectName("scrollAreaWidgetContents");
+        scrollAreaWidgetContents->setGeometry(QRect(0, 0, 429, 129));
+        labelObsCarro = new QLabel(scrollAreaWidgetContents);
+        labelObsCarro->setObjectName("labelObsCarro");
+        labelObsCarro->setGeometry(QRect(10, 10, 411, 111));
+        labelObsCarro->setFont(font2);
+        scrollArea->setWidget(scrollAreaWidgetContents);
+        commandButtonVoltar = new QCommandLinkButton(VerCarro);
+        commandButtonVoltar->setObjectName("commandButtonVoltar");
+        commandButtonVoltar->setGeometry(QRect(10, 10, 31, 41));
+        commandButtonVoltar->setCursor(QCursor(Qt::PointingHandCursor));
+        QIcon icon1;
+        icon1.addFile(QString::fromUtf8("../setavoltar.ico"), QSize(), QIcon::Normal, QIcon::Off);
+        commandButtonVoltar->setIcon(icon1);
+
+        retranslateUi(VerCarro);
+
+        QMetaObject::connectSlotsByName(VerCarro);
+    } // setupUi
+
+    void retranslateUi(QWidget *VerCarro)
+    {
+        VerCarro->setWindowTitle(QCoreApplication::translate("VerCarro", "Ver Carrro", nullptr));
+        btnDefinicoes->setText(QCoreApplication::translate("VerCarro", "Defini\303\247\303\265es", nullptr));
+        btnEstatistica->setText(QCoreApplication::translate("VerCarro", "Estat\303\255sticas", nullptr));
+        label->setText(QCoreApplication::translate("VerCarro", "Carro:", nullptr));
+        label_2->setText(QCoreApplication::translate("VerCarro", "Tipo:", nullptr));
+        label_3->setText(QCoreApplication::translate("VerCarro", "Observa\303\247\303\265es:", nullptr));
+        labelNomeCarro->setText(QCoreApplication::translate("VerCarro", "Carro XPTO", nullptr));
+        labelTipoCarro->setText(QCoreApplication::translate("VerCarro", "Eletrico", nullptr));
+        labelObsCarro->setText(QCoreApplication::translate("VerCarro", "Ano Xptodsajbjdkbk,a,bd", nullptr));
+        commandButtonVoltar->setText(QString());
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class VerCarro: public Ui_VerCarro {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_VERCARRO_H
